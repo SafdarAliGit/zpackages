65caabcf9e911519d674de9e4dd8359837a810c8
SafdarAliGit <safdar211@gmail.com>
2023-08-02 13:28:17 +0500

    first commit

---
+++ zpackages/zpackages/doctype/job_costing/job_costing.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Tech Ventures and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class JobCosting(Document):
+    pass
---
+++ zpackages/zpackages/doctype/process_wastage/test_process_wastage.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Tech Ventures and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProcessWastage(FrappeTestCase):
+	pass
