58645dac4ea6122679493c9c08791fb06ea3c2b9
SafdarAliGit <safdar211@gmail.com>
2023-08-11 19:26:02 +0500

    updated reprot

--- zpackages/zpackages/report/efs_report/efs_report.js
+++ zpackages/zpackages/report/efs_report/efs_report.js
@@ -1,19 +1,18 @@
-
 frappe.query_reports["EFS Report"] = {
     "filters": [
-
         {
             "fieldname": "customer",
             "label": __("Customer"),
             "fieldtype": "Link",
             "options": "Customer",
+            "reqd": 1  // Set the "reqd" attribute to make the filter mandatory
         },
         {
             "fieldname": "sales_order",
             "label": __("Sales Order"),
             "fieldtype": "Link",
-            "options": 'Sales Order'
+            "options": 'Sales Order',
+            "reqd": 1  // Set the "reqd" attribute to make the filter mandatory
         }
-    ],
-
+    ]
 };
