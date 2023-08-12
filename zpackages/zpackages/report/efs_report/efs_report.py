00b95f58bc40563bd6bbd7aafbc38e4e16557781
SafdarAliGit <safdar211@gmail.com>
2023-08-12 12:57:34 +0500

    updated

--- zpackages/zpackages/report/efs_report/efs_report.py
+++ zpackages/zpackages/report/efs_report/efs_report.py
@@ -214,7 +214,7 @@ def get_data(filters):
         WHERE
             {conditions}
         GROUP BY
-                  `tabSales Order Item`.parent,`tabRaw Items`.parent,`tabDelivery Note Item`.against_sales_order
+                  `tabSales Order Item`.parent
     """.format(conditions=get_conditions(filters, "Sales Order"))
 
     so_result = frappe.db.sql(so_query, filters, as_dict=1)
