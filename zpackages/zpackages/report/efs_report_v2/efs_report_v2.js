3ab7177e010f73d73c09b26dbb8d29371ce23fb7
SafdarAliGit <safdar211@gmail.com>
2023-08-29 20:48:07 +0500

    upated

---
+++ zpackages/zpackages/report/efs_report_v2/efs_report_v2.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Tech Ventures and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["EFS Report V2"] = {
+	"filters": [
+
+	]
+};
