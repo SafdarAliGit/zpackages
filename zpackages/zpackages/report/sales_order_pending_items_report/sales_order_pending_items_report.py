2b5f5efe551322b7abfe2f29ef3cd70742eae83a
SafdarAliGit <safdar211@gmail.com>
2023-09-14 23:18:56 +0500

    updated reports

--- zpackages/zpackages/report/sales_order_pending_items_report/sales_order_pending_items_report.py
+++ zpackages/zpackages/report/sales_order_pending_items_report/sales_order_pending_items_report.py
@@ -103,7 +103,6 @@ def get_data(filters):
             `tabSales Order`.docstatus <= 1
             AND 
             {conditions}
-        GROUP BY `tabSales Order Item`.item_code
     """.format(conditions=get_conditions(filters, "Sales Order"))
     sales_analytics_result = frappe.db.sql(sales_analytics, filters, as_dict=1)
     data.extend(sales_analytics_result)
