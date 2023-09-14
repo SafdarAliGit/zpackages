7cf7b1372ae50775eea70ca44429efd9d5d0a26c
SafdarAliGit <safdar211@gmail.com>
2023-09-14 21:11:46 +0500

    updated reports

--- zpackages/zpackages/report/sales_order_pending_items_report/sales_order_pending_items_report.py
+++ zpackages/zpackages/report/sales_order_pending_items_report/sales_order_pending_items_report.py
@@ -94,7 +94,7 @@ def get_data(filters):
             `tabSales Order Item`.description,
             `tabSales Order`.name AS sales_order_no,
             `tabSales Order Item`.item_code,
-            `tabSales Order Item`.qty AS so_qty,
+            SUM(`tabSales Order Item`.qty) AS so_qty,
             SUM(`tabDelivery Note Item`.qty) AS dn_qty,
             SUM(`tabSales Order Item`.qty - `tabDelivery Note Item`.qty) AS balance
         FROM 
@@ -113,6 +113,8 @@ def get_data(filters):
             {conditions}
             AND
             `tabSales Order Item`.qty != `tabDelivery Note Item`.qty 
+            AND
+            GROUP BY `tabSales Order Item`.description
     """.format(conditions=get_conditions(filters, "Sales Order"))
     sales_analytics_result = frappe.db.sql(sales_analytics, filters, as_dict=1)
     data.extend(sales_analytics_result)
