885bc7086248525c6b7894a75a0d87c10f128ee4
SafdarAliGit <safdar211@gmail.com>
2023-09-26 23:01:42 +0500

    update

---
+++ zpackages/zpackages/report/sales_summary_eval/sales_summary_eval.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2023, Tech Ventures and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Summary Eval"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "reqd": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "reqd": 1
+        }
+    ]
+};
---
+++ zpackages/zpackages/report/sales_summary_eval/sales_summary_eval.py
@@ -0,0 +1,114 @@
+# my_custom_app.my_custom_app.report.daily_activity_report.daily_activity_report.py
+import frappe
+from frappe import _
+
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+
+def decimal_format(value, decimals):
+    formatted_value = "{:.{}f}".format(value, decimals)
+    return formatted_value
+
+
+def get_columns():
+    columns = [
+        {
+            "label": _("Item Group"),
+            "fieldname": "item_group",
+            "fieldtype": "Link",
+            "options": 'Item Group',
+            "width": 150
+        },
+        {
+            "label": _("Voucher #"),
+            "fieldname": "name",
+            "fieldtype": "Link",
+            "options": 'Delivery Note',
+            "width": 150
+        }
+        ,
+        {
+            "label": _("Status"),
+            "fieldname": "status",
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "label": _("Customer"),
+            "fieldname": "customer",
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 150
+        }
+        ,
+
+        {
+            "label": _("Customer Name"),
+            "fieldname": "customer_name",
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "label": _("Grand Total"),
+            "fieldname": "base_grand_total",
+            "fieldtype": "Currency",
+            "width": 150
+        },
+
+    {
+        "label": _("Net Total"),
+        "fieldname": "net_total",
+        "fieldtype": "Currency",
+        "width": 150
+    },
+
+        {
+            "label": _("Taxes And Charges"),
+            "fieldname": "base_total_taxes_and_charges",
+            "fieldtype": "Currency",
+            "width": 150
+        }
+
+    ]
+    return columns
+
+
+def get_conditions(filters, doctype):
+    conditions = []
+
+    if filters.get("from_date"):
+        conditions.append(f"`tab{doctype}`.posting_date >= %(from_date)s")
+    if filters.get("to_date"):
+        conditions.append(f"`tab{doctype}`.posting_date <= %(to_date)s")
+
+    return " AND ".join(conditions)
+
+
+def get_data(filters):
+    data = []
+    sales_summary = """SELECT `tabDelivery Note`.name,
+            `tabDelivery Note`.customer, 
+            `tabDelivery Note`.status,
+            `tabDelivery Note`.customer_name,
+            `tabDelivery Note`.base_grand_total,
+            `tabDelivery Note`.net_total, 
+            `tabDelivery Note`.base_total_taxes_and_charges,
+            `tabDelivery Note Item`.item_group as item_group
+        FROM
+            `tabDelivery Note`
+        LEFT JOIN
+            `tabDelivery Note Item` ON `tabDelivery Note`.name = `tabDelivery Note Item`.parent
+        WHERE 
+            `tabDelivery Note`.status != 'Cancelled' AND
+            {conditions}
+        ORDER BY `tabDelivery Note`.modified DESC
+    """.format(conditions=get_conditions(filters, "Delivery Note"))
+
+    sales_summary_result = frappe.db.sql(sales_summary, filters, as_dict=1)
+    data.extend(sales_summary_result)
+    return data
+
