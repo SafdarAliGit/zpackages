a523ee03ce575f2396cf2ca93c9755b7129417fb
safdar211ali <safdar211@hotmail.com>
2025-07-30 16:51:10 +0500

    updated

---
+++ zpackages/zpackages/doctype/hs_code/hs_code.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2025, Tech Ventures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('HS Code', {
+	refresh: function(frm) {
+		frm.set_query("parent_hs_code", function() {
+			return {
+				filters: {
+					"is_group": 1
+				}
+			}
+		});
+	}
+});
