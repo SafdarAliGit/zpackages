5425a4fa71a228e9f95532e0aa6bd7c143824139
safdar211ali <safdar211@hotmail.com>
2025-07-30 16:43:16 +0500

    updated

--- zpackages/zpackages/doctype/hs_code/hs_code.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Tech Ventures and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.utils.nestedset import NestedSet
-
-class HSCode(NestedSet):
-	pass
